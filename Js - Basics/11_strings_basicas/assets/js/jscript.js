5c5e0eae4df92feabc67674060ed6ee077ce28e4
David Monteiro <dmonn97@gmail.com>
2026-02-23 17:08:10 -0300

    Strings

---
+++ Js - Basics/11_strings_basicas/assets/js/jscript.js
@@ -0,0 +1,10 @@
+const meuNome = 'David';
+let idade = 28;
+idade = idade + 1;
+//Concatenacao 1
+const mensagem = 'David tem ' + idade + ' anos de idade';
+
+//Concatenacao 2
+const mensagem2 = `${meuNome} tem ${idade} anos de idade`;
+
+console.log(mensagem);
