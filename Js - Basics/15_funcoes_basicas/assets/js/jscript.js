1eec3eef7a7fce91637d0132c0178cc16084de7a
David Monteiro <dmonn97@gmail.com>
2026-03-15 01:10:38 -0300

    Funcoes Basicas[F

---
+++ Js - Basics/15_funcoes_basicas/assets/js/jscript.js
@@ -0,0 +1,14 @@
+// //Sem funcao
+// let a = 8;
+// let b = 9;
+
+// let area = a * b;
+
+// console.log(area); 
+
+function calcArea(a,b) {
+    let area = a * b;
+    return area;
+}
+
+console.log(calcArea(5,5));
