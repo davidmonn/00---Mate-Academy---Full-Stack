0011751522a5e1cd611c881cecabef3f62160e6c
David Monteiro <dmonn97@gmail.com>
2026-03-15 01:47:09 -0300

    Funcion

---
+++ Js - Basics/16_function/assets/js/jscript.js
@@ -0,0 +1,8 @@
+function greeterUser(name, partOfTheDay) {
+    let message = `God ${partOfTheDay}, ${name}!`;
+    console.log(message);
+}
+
+greeterUser('David', 'Morning');
+greeterUser('Suzana', 'Afternoon');
+greeterUser('Toninha', 'Night');
