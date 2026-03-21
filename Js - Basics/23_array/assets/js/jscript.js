05d94b310b2a01ef197994303cf71cded4bd1314
David Monteiro <dmonn97@gmail.com>
2026-03-21 04:06:52 -0300

    Array

---
+++ Js - Basics/23_array/assets/js/jscript.js
@@ -0,0 +1,7 @@
+let city = ['Sao Paulo', 'Guarulhos', 'Liberdade', 'Bras', 'Leme'];
+let distances = [10, 30, 45, 300, 15];
+let person = ['David', 28, false];
+
+console.log(city);
+console.log(distances);
+console.log(person);
