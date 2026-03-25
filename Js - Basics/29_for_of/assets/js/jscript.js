7e6c9d0a4fbaf0b2eecb91c005cf52165e8ca5bb
David Monteiro <dmonn97@gmail.com>
2026-03-25 02:13:41 -0300

    att

--- Js - Basics/29_for_of/assets/js/jscript.js
+++ Js - Basics/29_for_of/assets/js/jscript.js
@@ -1,8 +1,8 @@
 let title = 'Trabalhando com Strings';
 
-// Essa função percorre a string caractere por caractere,
-// substituindo todas as ocorrências de um caractere específico
-// por outro valor definido
+// // Essa função percorre a string caractere por caractere,
+// // substituindo todas as ocorrências de um caractere específico
+// // por outro valor definido
 function replaceAll(input, char, replacement) {
     let result = '';
     for(let ch of input) {
@@ -16,3 +16,40 @@ function replaceAll(input, char, replacement) {
 }
 
 console.log(replaceAll(title, ' ', '-'));
+
+/**
+ * This comment gives you autocompletion
+ * 
+ * param {string} input
+ */
+
+// function replaceSpaces(input) {
+//   let result = '';
+
+//   // Write code here
+//   for(let i = 0; i <= input.length - 1; i++) {
+//     if(input[i] === ' ') {
+//         result += '-';
+//     } else {
+//         result += input[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+function replaceSpaces(input) {
+  let result = '';
+
+  for(let i = 0; i < input.length; i++) {
+    if(input[i] === ' ') {
+        result += '-';
+    } else {
+        result += input[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(replaceSpaces('testando teste'));
