62e3d16e90559a7b1f65c71abebac901b6c9d8e0
David Monteiro <dmonn97@gmail.com>
2026-04-04 16:49:12 -0300

    Number Literals

---
+++ 02_javascript_basics_extended/03_trabalhando_com_numeros/01_number_literals/assets/js/jscript.js
@@ -0,0 +1,18 @@
+let x1 = 42;
+
+let x2 = 0b101010;
+let x3 = 0o52;
+let x4 = 0x2A;
+let x5 = 0x2a;
+
+console.log(x1, x2x, x3, x4, x5);
+
+let y1 = 42.000;
+let y2 = 42.;
+
+let y3 = 0.42e2; // 0.42 * (10 ** 2) => 42
+let y4 = .42e2;
+
+let y5 = 42000e-3; // 42000 / (10 ** 3  ) => 42
+
+console.log(y1, y2, y3, y4, y5);
