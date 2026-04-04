c81bbb01a3efe436d542c90287cfab4ce579c928
David Monteiro <dmonn97@gmail.com>
2026-04-04 17:25:47 -0300

    Numbers Binary and Hex

---
+++ 02_javascript_basics_extended/03_trabalhando_com_numeros/02_numbers_binary_and_hex/assets/js/jscript.js
@@ -0,0 +1,16 @@
+const decimal = 111; // Base 10
+//111 = 1*100 + 1 * 10 + 1*1
+
+const binario = 0b111; // Base 2
+//111 = 1*4 + 1*2 + 1*1 = 7
+
+const hexa = 0x111; // Base 16
+//111 = 1*256 + 1*16 + 1*1 = 276
+
+console.log(`111 Decimal: ${decimal}`);
+console.log(`111 Binario ${binario}`);
+console.log(`111 Hexa: ${hexa}`);
+
+
+const numero = 123; //Decimal
+console.log(numero.toString(16));
