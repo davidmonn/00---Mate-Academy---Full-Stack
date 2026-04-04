4e3dd71cb6eb4a741f7b73e771047911b53e5c2f
David Monteiro <dmonn97@gmail.com>
2026-04-04 18:05:26 -0300

    Number Operators

---
+++ 02_javascript_basics_extended/03_trabalhando_com_numeros/03_number_operators/assets/js/jscript.js
@@ -0,0 +1,8 @@
+console.log(
+    10 + 8, // Soma = 18
+    10 - 8, // Subtracao = 2
+    10 * 8, // Multiplicacao = 80
+    10 / 8, // Divisao = 1.25
+    10 % 8, //(Modulo) Resto da Divisao = 2
+    10 ** 8, // Exponencial  100000000
+)
