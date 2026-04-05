69e880b20d68df6d41e45bf0d040399e9741803b
David Monteiro <dmonn97@gmail.com>
2026-04-05 17:23:04 -0300

    Tarefa numeral systems

---
+++ 02_javascript_basics_extended/03_trabalhando_com_numeros/10_tarefa_numeral_systems/assets/js/jscript.js
@@ -0,0 +1,17 @@
+// write code below
+let myBin = 0b1010;
+// myBin.toString(2);
+
+let myOct = 0o24;
+// myOct.toString(8);
+
+let myDec = 30;
+// myDec.toString(10);
+
+let myHex = 0x28;
+// myHex.toString(16);
+
+// console.log(myBin.toString(2));
+// console.log(myOct.toString(8));
+// console.log(myDec.toString(10));
+// console.log(myHex.toString(16));
