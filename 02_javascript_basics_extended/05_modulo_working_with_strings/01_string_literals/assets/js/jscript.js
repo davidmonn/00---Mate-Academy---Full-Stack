1d5378899d5f7955de35640e0128acf454a09d0b
David Monteiro <dmonn97@gmail.com>
2026-04-12 19:38:54 -0300

    Working with Strings

---
+++ 02_javascript_basics_extended/05_modulo_working_with_strings/01_string_literals/assets/js/jscript.js
@@ -0,0 +1,9 @@
+const meuNome = 'David';
+const sobNome = 'Monteiro';
+//Template string - Interpolacao, fica mais simples quando precisamos adicionar
+// ou injetar alguma string sem concatenacao
+const meuNomeCompleto = `${meuNome} ${sobNome} Matias Adao`;
+
+console.log(meuNome);
+console.log(sobNome);
+console.log(meuNomeCompleto);
