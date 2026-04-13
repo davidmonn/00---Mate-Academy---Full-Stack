9eb17ccc9afdb6c5743957d3667ad5a9ebf24f52
David Monteiro <dmonn97@gmail.com>
2026-04-13 16:57:35 -0300

    HTML Strings

---
+++ 02_javascript_basics_extended/05_modulo_working_with_strings/04_html_strings/assets/js/jscript.js
@@ -0,0 +1,17 @@
+const s1 = '<div class="spinner">Carregando...</div>';
+
+console.log(s1);
+
+const a = 2;
+const b = 5;
+
+const s2 = `
+<h1>Titulo</h1>
+<ul>
+    <li>${a} + ${b} = ${a + b}</li>
+    <li>${a} * ${b} = ${a * b}</li>
+    <li>${a} / ${b} = ${a / b}</li>
+</ul>
+`;
+
+console.log(s2);
