af5843cffc30c330ae15a67c1392498da61f15da
David Monteiro <dmonn97@gmail.com>
2026-04-13 15:46:19 -0300

    Concatenation vs Interpolation

---
+++ 02_javascript_basics_extended/05_modulo_working_with_strings/02_concatenation_vs_interpolation/assets/js/jscript.js
@@ -0,0 +1,10 @@
+const nome = 'David';
+
+//Concatenacao
+const mensagem1 = 'Ola ' + nome + '!';
+
+//Interpolacao/Template String
+const mensagem2 = `Ola, ${nome}!`;
+
+console.log(mensagem1);
+console.log(mensagem2);
