1d757990cb696181b124ff2ec4987c57690bbe07
David Monteiro <dmonn97@gmail.com>
2026-04-13 18:09:22 -0300

    String Case Swapping

---
+++ 02_javascript_basics_extended/05_modulo_working_with_strings/06_string_case_swapping/assets/js/jscript.js
@@ -0,0 +1,8 @@
+const palavra = 'Ola, mundo!';
+
+const palavraMaiuscula = palavra.toUpperCase(palavra);
+const palavraMinuscula = palavra.toLowerCase(palavra);
+
+console.log(palavra);
+console.log(palavraMaiuscula);
+console.log(palavraMinuscula);
