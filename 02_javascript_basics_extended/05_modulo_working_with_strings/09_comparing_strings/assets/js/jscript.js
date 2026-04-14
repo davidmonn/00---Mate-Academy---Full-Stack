2177cd7270be5a8be22d9886107122798e8052cd
David Monteiro <dmonn97@gmail.com>
2026-04-14 15:48:13 -0300

    Comparing strings

---
+++ 02_javascript_basics_extended/05_modulo_working_with_strings/09_comparing_strings/assets/js/jscript.js
@@ -0,0 +1,15 @@
+const nome1 = 'Suzana';
+const nome2 = 'david';
+
+console.log(
+    nome1,
+    nome2,
+    nome1.localeCompare(nome2)
+);
+
+console.log(
+    nome1[0],
+    nome1.charCodeAt(0),
+    nome2[0],
+    nome2.charCodeAt(0)
+)
