6e90e508a60f9aa5a0a888e0946943454db9b0a6
David Monteiro <dmonn97@gmail.com>
2026-04-15 19:50:11 -0300

    Amanha

---
+++ treinos_exercicios_ia/11_treino_curso_jumping_numbers(refazer)/assets/js/jscript.js
@@ -0,0 +1,3 @@
+function isJumping(n) {
+   
+}
---
+++ treinos_exercicios_ia/12_treino_curso_make_abbr_(refazer)/assets/js/jscript.js
@@ -0,0 +1,18 @@
+/*
+// 'NASA'
+makeAbbr('national aeronautics space administration');
+
+// 'CPU'
+makeAbbr('central processing unit');
+
+// 'SMILES'
+makeAbbr('simplified molecular input line entry specification');
+*/
+
+const test = 'national aeronautics space administration';
+
+function makeAbbr(words) {
+    
+}
+
+console.log(makeAbbr(test));
