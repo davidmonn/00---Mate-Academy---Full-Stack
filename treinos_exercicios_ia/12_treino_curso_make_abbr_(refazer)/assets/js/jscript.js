3b282e5947d18c13aaad4aa2a31647a822ae2984
David Monteiro <dmonn97@gmail.com>
2026-04-16 16:37:24 -0300

    Treino: Exercicio string

--- treinos_exercicios_ia/12_treino_curso_make_abbr_(refazer)/assets/js/jscript.js
+++ treinos_exercicios_ia/12_treino_curso_make_abbr_(refazer)/assets/js/jscript.js
@@ -9,10 +9,41 @@ makeAbbr('central processing unit');
 makeAbbr('simplified molecular input line entry specification');
 */
 
+
+// For
 const test = 'national aeronautics space administration';
 
-function makeAbbr(words) {
-    
+function abreviation(chars) {
+    let store = chars[0];
+
+    for(let char = 0; char < chars.length; char++) {
+        let storeChar = chars[char];
+
+        if(storeChar === ' ') {
+            store += chars[char + 1];
+        }
+    }
+    return store.toUpperCase();
+}
+console.log(abreviation(test));
+
+
+
+// For Of
+const test2 = 'national aeronautics space administration';
+
+function abreviar(letras) {
+    let guardarLetras = letras[0];
+    let i = 0;
+
+    for(let letra of letras) {
+        if(i > 0 && letras[i - 1] === ' ') {
+            guardarLetras += letra;
+        }
+
+        i++;
+    }
+    return guardarLetras.toUpperCase();
 }
 
-console.log(makeAbbr(test));
+console.log(abreviar(test2));
