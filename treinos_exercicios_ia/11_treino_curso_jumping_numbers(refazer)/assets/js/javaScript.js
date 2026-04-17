c077d44be8239272843721346a9f66edc2a4c9da
David Monteiro <dmonn97@gmail.com>
2026-04-17 16:48:41 -0300

    treino

--- treinos_exercicios_ia/11_treino_curso_jumping_numbers(refazer)/assets/js/javaScript.js
+++ treinos_exercicios_ia/11_treino_curso_jumping_numbers(refazer)/assets/js/javaScript.js
@@ -1,88 +1,3 @@
-const test = 123;
-
-function crescente(number) {
-    number = number.toString();
-    let ind = 0;
-
-    for(let i of number) {
-        const iterar = Number(i);
-        const iterarSub = Number(number[ind - 1]);
-
-        if(ind > 0) {
-            if(Math.abs(iterar - iterarSub) != 1) {
-                return 'NOT JUMPING';
-            }
-        }
-        ind++;
-    }
-    return 'JUMPING';
-}
-
-console.log(crescente(test));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function crescente(valores) {
-    let convertStr = valores.toString();
-
-    for(let valor = 1; valor < convertStr.length; valor++) {
-        let num = Number(convertStr[valor]);
-        let numSub = Number(convertStr[valor - 1]);
-
-        if(num + 1 != numSub && num - 1 != numSub) {
-            return 'NOT JUMPING';
-        }
-    }
-    return 'JUMPING';
-}
-
-console.log(crescente(test));
-*/
-
-
-/*
 // For
 const test = '123';
 
@@ -108,7 +23,24 @@ console.log(jumpNumber(test));
 //For of
 const test2 = '321';
 
+function numerosCerto(numeros) {
+    numeros = numeros.toString();
+    let indice = 0;
 
+    for(let numero of numeros) {
+        if(indice > 0) {
+            let iterar = Number(numero);
+            let iterarSub = Number(numeros[indice - 1]);
+
+            if(iterar + 1 !== iterarSub && iterar - 1 !== iterarSub) {
+                return 'NOT JUMPING';
+            }
+        }
+
+        indice++;
+    }
+
+    return 'JUMPING';
+}
 
 console.log(numerosCerto(test2));
-*/
