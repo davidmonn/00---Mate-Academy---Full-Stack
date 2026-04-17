b4878150ee7ab930d7f0cd23e8f3e3f4d790bcdf
David Monteiro <dmonn97@gmail.com>
2026-04-17 14:08:59 -0300

    treino

--- treinos_exercicios_ia/13_exercicio_abreviar_string/assets/js/jscript.js
+++ treinos_exercicios_ia/13_exercicio_abreviar_string/assets/js/jscript.js
@@ -1,14 +1,29 @@
-const test = 'Hello Word';
+// const test = 'Hello Word';
 
-function firstChars(chars) {
-    let words = chars.split(' ');
-    let result = '';
+// function firstChars(chars) {
+//     let words = chars.split(' ');
+//     let result = '';
 
-    for (let word of words) {
-        result += word[0] + word[word.length - 1] + ' ';
-    }
+//     for (let word of words) {
+//         result += word[0] + word[word.length - 1] + ' ';
+//     }
 
-    return result.trim();
+//     return result.trim();
+// }
+
+// console.log(firstChars(test));
+
+/*
+Crie uma função chamada multiply.
+Passe os parâmetros a e b para a função.
+Retorne o produto dos parâmetros da função.
+Se o parâmetro b não for passado para a função, use 1 como valor padrão.
+*/
+
+function multiply(a = 1,b = 1) {
+    const result = a * b;
+
+    return result;
 }
 
-console.log(firstChars(test));
+console.log(multiply(24))
