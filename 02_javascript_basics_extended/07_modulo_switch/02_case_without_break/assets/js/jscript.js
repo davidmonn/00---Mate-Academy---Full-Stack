172282167e00c507d5efbec4a5125f87c7e6c3a1
David Monteiro <dmonn97@gmail.com>
2026-04-20 17:07:23 -0300

    Case Without Break

---
+++ 02_javascript_basics_extended/07_modulo_switch/02_case_without_break/assets/js/jscript.js
@@ -0,0 +1,18 @@
+const contador = 3;
+
+switch (contador) {
+    case 1:
+        console.log('Um');
+        break;
+    case 2:
+        console.log('Dois');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Alguns');
+        break;
+    default:
+        ('Muitos');
+}
+
