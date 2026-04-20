c14c51207d38814b731185d68075f9d123053abd
David Monteiro <dmonn97@gmail.com>
2026-04-20 15:01:47 -0300

    Arrow Function

---
+++ 02_javascript_basics_extended/06_modulo_functions_revisited/03_arrow_function/assets/js/jscript.js
@@ -0,0 +1,19 @@
+//Antes de seguir as regras ficaria assim:
+// const obterAreaDoCirculo = (raio) => {
+//     return Math.PI * raio * raio;
+// }
+
+
+
+//Com regra
+const obterAreaDoCirculo1 = (raio) => Math.PI * raio * raio;
+const area = obterAreaDoCirculo1(1);
+console.log(area);
+
+
+
+//Com um Parametro
+const obterAreaDoCirculo2 = (raio) => Math.PI * raio * raio;
+const area2 = obterAreaDoCirculo2(1)
+
+console.log(area2);
