134aabbde4ad9d926c709f6424db1a9702b9f4cf
David Monteiro <dmonn97@gmail.com>
2026-04-21 17:53:31 -0300

    Conversion to Number

---
+++ 02_javascript_basics_extended/08_modulo_type_conversion/02_conversion_to_number/assets/js/jscript.js
@@ -0,0 +1,7 @@
+const valor = '100asdad';
+
+console.log(Number(valor), 'Number'); // Faz conversao de str para numero
+console.log(+valor, '+'); // Faz conversao de str para numero
+
+console.log(parseFloat(valor), 'parseFloat');
+console.log(parseInt(valor), 'parseInt');
