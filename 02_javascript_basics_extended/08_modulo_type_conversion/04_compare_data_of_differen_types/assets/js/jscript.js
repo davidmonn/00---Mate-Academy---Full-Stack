897cf26249a0b8b06afb036666cf8b61fd32d11b
David Monteiro <dmonn97@gmail.com>
2026-04-22 15:14:06 -0300

    Compare data of different types

---
+++ 02_javascript_basics_extended/08_modulo_type_conversion/04_compare_data_of_differen_types/assets/js/jscript.js
@@ -0,0 +1,9 @@
+// console.log(2 > '18');
+// console.log('2' > 18);
+// console.log('2' > '18');
+
+
+console.log(2 == '\t 2. \n ');
+console.log(1 == true);
+console.log(0 == null);
+console.log(null == undefined);
