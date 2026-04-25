cf17a7de493913bdbd2dcd76d8cf2414c19a9fb2
David Monteiro <dmonn97@gmail.com>
2026-04-25 16:50:22 -0300

    Object reading props with brackets

---
+++ 02_javascript_basics_extended/10_modulo_object_basics/06_object_reading_props_with_brackets/assets/js/jscript.js
@@ -0,0 +1,13 @@
+const user = {
+    nome: 'David',
+    sobnome: 'Monteiro',
+    idade: 28,
+};
+
+const sobnome = 'sobnome';
+
+console.log(
+    user['nome'],
+    user[sobnome],
+    user.idade,
+)
