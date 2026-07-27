c39ff828cc111e05f6ce8a5f205e67317926142b
David Monteiro <dmonn97@gmail.com>
2026-07-27 13:54:44 -0300

    null vs undefined

---
+++ 06_javascript_advanced/02_object_advanced/03_null_vs_undefined/assets/js/jscript.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const div = document.querySelector('div');
+
+console.log(div);
+console.log(div.previousElementSibling);
+console.log(div.prevElementSibling);
+
+const user = {
+    age: 0,
+    name: '',
+    passaport: null,
+    friends: [],
+};
+
+console.log(user.passaport);
