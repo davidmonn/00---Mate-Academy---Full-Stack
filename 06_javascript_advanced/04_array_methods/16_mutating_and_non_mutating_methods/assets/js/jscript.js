8cbbe6304facfb296ec62a47a3bb8381e774b5bd
David Monteiro <dmonn97@gmail.com>
2026-08-21 14:49:56 -0300

    Mutating and Non-mutating Methods

---
+++ 06_javascript_advanced/04_array_methods/16_mutating_and_non_mutating_methods/assets/js/jscript.js
@@ -0,0 +1,31 @@
+const numbers = [3, 1, 12, 5, 2, 3, 4];
+
+let result;
+
+console.log(numbers);
+
+/* MUTATING METHODS */
+
+// result = numbers.push(100, 200, 300);
+// reuslt = numbers.unshift(100, 200, 300);
+// result = numbers.pop();
+// result = numbers.shift();
+// result = numbers.splice(2, 5);
+// result = numbers.fill('a');
+// result = numbers.reverse();
+// result = numbers.sort();
+
+/* NON MUTATING METHODS */
+// result = numbers.includes(3);
+// result = numbers.indexOf(3);
+// result = numbers.lastIndexOf(3);
+// result = numbers.join('-');
+// result = numbers.slice(2, 5);
+// result = numbers.concat(100, ['x', 'y'], 200, 300);
+
+console.log('-------------');
+// 
+
+
+console.log(numbers);
+console.log(result);
