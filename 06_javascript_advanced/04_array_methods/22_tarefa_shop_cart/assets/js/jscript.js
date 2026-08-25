b466dcdd6757e74ca7f4f74607cf9709bb825980
David Monteiro <dmonn97@gmail.com>
2026-08-25 16:34:14 -0300

    Tarefa: Shop cart

---
+++ 06_javascript_advanced/04_array_methods/22_tarefa_shop_cart/assets/js/jscript.js
@@ -0,0 +1,26 @@
+// const order = '1 coca cola, 5 chicken nuggets, 20 egg';
+const order = '';
+
+function makeOrderList(order) {
+  const itens = order.split(',');
+  const list = {};
+
+  if (order === '') {
+    return list;
+  }
+
+  for (let item = 0; item < itens.length; item++) {
+    const currentItem = itens[item].trim();
+    const position = currentItem.indexOf(' ');
+    const quant = Number(currentItem.slice(0, position));
+    const product = currentItem.slice(position + 1).split(' ').join('_');
+
+    list[product] = quant;
+  }
+
+  return list;
+}
+
+
+const list = makeOrderList(order);
+console.log(list);
