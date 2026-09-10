82f85b08225b74902b79ab2caa2f6943b00e617b
David Monteiro <dmonn97@gmail.com>
2026-09-10 13:13:55 -0300

    Scope and Lexical Environment

---
+++ 06_javascript_advanced/08_closures/01_scope_and_lexical_environment/assets/js/jscript.js
@@ -0,0 +1,21 @@
+// console.log(window.Math);
+
+// #Script = { a: 1 }
+let a = 1;
+
+if (true) {
+  // #LE_1 = { b: 2 }
+  let b = 2;
+
+  if (true) {
+    // #LE_2 = { c: 3}
+    let c = 3;
+
+    var aa = 1;
+    function aaa() {};
+  };
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+};
